2f2d57e6c43d8f6e0885275ce49b97e222958d4e
yellow huang <yellow.huang@fikapark.com>
2022-06-29 10:12:51 +0800

    fika-iot-gateway: hotplug/init & related certifacate for MVP_000001

--- net/fika-iot-gateway/fika-iot-gateway.mk
+++ net/fika-iot-gateway/fika-iot-gateway.mk
@@ -32,4 +32,21 @@ endef
 
 FIKA_IOT_GATEWAY_POST_RSYNC_HOOKS += MANUAL_PATCH
 
+FIKA_IOT_GATEWAY_MY_DIR=package/longdong/net/fika-iot-gateway/files
+FIKA_IOT_GATEWAY_MY_FILES=fika_iot_gateway.yaml MVP_000001-certificate.pem.crt MVP_000001-private.pem.key AmazonRootCA1.pem AmazonRootCA3.pem
+define FIKA_IOT_GATEWAY_INSTALL_MISC
+	$(INSTALL) -m 0755 -D $(FIKA_IOT_GATEWAY_MY_DIR)/fika_iot_gateway.init \
+		$(TARGET_DIR)/etc/init.d/S80fika-iot-gateway
+	$(INSTALL) -m 0755 -D $(FIKA_IOT_GATEWAY_MY_DIR)/fika-iot-gateway.hotplug \
+		$(TARGET_DIR)/etc/hotplug.d/iface/90-fika-iot-gateway
+	$(INSTALL) -d $(TARGET_DIR)/etc/fika_iot_gateway
+	for i in $(FIKA_IOT_GATEWAY_MY_FILES); do \
+		$(INSTALL) -m 0644 -D $(FIKA_IOT_GATEWAY_MY_DIR)/$${i} \
+			$(TARGET_DIR)/etc/fika_iot_gateway/$${i}; \
+	done
+endef
+
+FIKA_IOT_GATEWAY_POST_INSTALL_TARGET_HOOKS += FIKA_IOT_GATEWAY_INSTALL_MISC
+
+
 $(eval $(cmake-package))
