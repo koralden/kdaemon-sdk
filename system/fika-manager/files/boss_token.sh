05a13ec480db9b801d4492817efc47bff78b8b81
Yellow Huang <yellow.huang@fikapark.com>
2022-07-29 16:51:59 +0800

    fika-manager: complete honest-challenge get/post flow     * jira - https://koralden.atlassian.net/browse/WIFIMVP-54?atlOrigin=eyJpIjoiMjRkMDliNTY1MmQ1NDExNWI5MGUwZTYxMDJhNTY4NzQiLCJwIjoiaiJ9     * BOSS POST function WIP

--- system/fika-manager/files/boss_token.sh
+++ system/fika-manager/files/boss_token.sh
@@ -2,17 +2,9 @@
 
 DEBUG=0
 
-my_log() {
-    level=$1 && shift
+source /etc/fika_manager/hcs_honest_challenge.sh
 
-    if [ -e /dev/log ]; then
-        logger -s -t fika-manager -p ${level} "$@"
-    else
-        echo "[fika-manager][${level}] $@"
-    fi
-}
-
-get_access_token_ap() {
+get_boss_ap_token() {
     local atoken=$1 && shift
     local url=$1 && shift
     local json
@@ -21,116 +13,101 @@ get_access_token_ap() {
     #my_log debug "curl ap_token $json"
 
     code=$(echo $json | jq -r .code)
-    if [ $code -eq 200 ]; then
+    if [ "X$code" = "X200" ]; then
         apToken=$(echo $json | jq -r .ap_token)
         #my_log debug "apToken as $apToken"
         echo "$apToken"
     #else
-        #my_log error "NO B/OSS ACCESSTOKEN-AP because ${response}, force exit" 
+        #my_log error "NO B/OSS ACCESSTOKEN-AP in ${url}, force exit" 
         #exit 127
     fi
 }
 
 post_challenger() {
-    sid=$1 && shift
-    challengeId=$1 && shift
+    local response challenger hashed json code
+    local cid
 
-    challenger=$(redis-cli --raw HGET boss.hcs.challengers.${sid} ${challengeId})
+    sid=$1 && shift
+    cid=$1 && shift
+
+    #my_log debug "[post_challenger]: sid=$sid challengeId=$challengeId"
+
+    challenger=$(redis-cli --raw HGET boss.hcs.challengers.${sid} ${cid})
     hashed=$(echo $challenger | jq -r .hashed)
 
-    json=$(jq -rcM --null-input \
-        --arg wallet "$challengeId" \
-        --arg hashed "$hashed" \
-        '{ "app_wallet": $wallet, "hashed": $hashed}')
-
-    response=$(curl -s -H "ACCESSTOKEN:${accesstoken}" -H "ACCESSTOKEN-AP:${accesstokenAp}" -X POST -d $json "${rootUrl}/${hcsPath}?ap_wallet=${kapWallet}")
-    my_log debug $response
-    code=$(echo $response | jq -r .code)
-    if [ $code -eq 200 ]; then
-        changed=$(echo $challenger | jq -rcM --argjson send true '.send = $send')
-        redis-cli HDEL boss.hcs.challengers.${sid} ${challengeId}
-        my_log debug "redis-cli HDEL boss.hcs.challengers.${sid} ${challengeId}"
-    else
-        my_log error "POST $json ${rootUrl}/${hcsPath}?ap_wallet=${kapWallet} fail"
-    fi
+    report_boss_hcs "$cid" "$hashed" "$sid"
 }
 
 remove_expired_task() {
     now=$(date +%s)
-    redis-cli lrange boss.hcs.token.list 0 -1 |
-        while read item; do
-            [ -z "$item" ] && break
-            invalidT=$(echo $item | jq -r .invalid_time)
-            invalidS=$(date -d "$(echo ${invalidT} | sed -e 's,T, ,' -e 's,+.*$,,')" +%s)
-            sid=$(echo $item | jq -r .hcs_sid)
+    for item in $(redis-cli LRANGE boss.hcs.token.list 0 -1); do
+        #my_log debug "[remove_expired_task]: item=$item"
+        [ -z "$item" ] && break
+        invalidT=$(echo $item | jq -r .invalid_time)
+        invalidS=$(date -d "$(echo ${invalidT} | sed -e 's,T, ,' -e 's,+.*$,,')" +%s)
+        sid=$(echo $item | jq -r .hcs_sid)
 
-            if [ $now -ge $invalidS ]; then
-                redis-cli HKEYS boss.hcs.challengers.${sid} | \
-                    while read cc; do
-                        post_challenger $sid $cc
-                    done
-                redis-cli LPOP boss.hcs.token.list
-                redis-cli DEL boss.hcs.challengers.${sid}
-                continue
-            fi
-            break
-        done
+        #my_log debug "[remove_expired_task]: invalidT=$invalidT invalidS=$invalidS sid=$sid vs now=$now"
+
+        if [ $now -ge $invalidS ]; then
+            for cc in $(redis-cli HKEYS boss.hcs.challengers.${sid}); do
+                post_challenger $sid $cc
+            done
+            redis-cli LPOP boss.hcs.token.list
+            redis-cli DEL boss.hcs.challengers.${sid}
+            continue
+        fi
+        break
+    done
 }
 
 # curl -s -H 'ACCESSTOKEN:ce18d7a0940719a00da82448b38c90b2' -X GET "https://oss-api.k36588.info/v0/ap/ap_token?ap_wallet=0x365962cd383a593975E743fD59Bbf2C1Bc141CF5"
 #{"ap_token":"02ec7a905b70689d9b30c6118fd1e62f","code":200}⏎ 
 
-kap_core=$(redis-cli --raw GET kap.core)
-kap_boss=$(redis-cli --raw GET kap.boss)
-
-rootUrl=$(echo $kap_boss | jq -r .root_url)
-accesstoken=$(echo $kap_boss | jq -r .access_token)
-apTokenPath=$(echo $kap_boss | jq -r .ap_token_path)
-hcsPath=$(echo $kap_boss | jq -r .hcs_path)
-kapWallet=$(echo $kap_core | jq -r .wallet_address)
-accesstokenAp=$(redis-cli --raw GET kap.boss.ap.token)
-
-my_log debug "appUrl: $rootUrl"
-my_log debug "kapWallet: $kapWallet"
-my_log debug "accesstoken: $accesstoken"
-my_log debug "accesstokenAp: $accesstokenAp"
-
 #[ $DEBUG -eq 1 ] && redis-cli SET kap.boss.ap.token 02ec7a905b70689d9b30c6118fd1e62f
 #if [ -z "$accesstokenAp" ]; then
-#    accesstokenAp=$(get_access_token_ap $accesstoken "${rootUrl}/${apTokenPath}?ap_wallet=${kapWallet}")
+#    accesstokenAp=$(get_boss_ap_token $accesstoken "${rootUrl}/${apTokenPath}?ap_wallet=${kapWallet}")
 #    redis-cli SET kap.boss.ap.token $accesstokenAp
 #else
 #    my_log info "api accesstoken-ap as $accesstokenAp"
 #fi
 
-[ -z "$rootUrl" -o -z "$accesstoken" -o -z "$accesstokenAp" -o -z "$kapWallet" ] && exit 127
+get_main() {
+    local tasks code hcs hashOne
 
-remove_expired_task
+    db_fetch
 
-response=$(curl -s -H "ACCESSTOKEN:${accesstoken}" -H "ACCESSTOKEN-AP:${accesstokenAp}" -X GET "${rootUrl}/${hcsPath}?ap_wallet=${kapWallet}")
-[ $DEBUG -eq 1 ] && response='{"hcs":[{"hcs_sid":"2022072215014100001","hcs_token":"94efde4c624ce129eab0756b52897de3","init_time":"2022-07-22T15:01:41+0800","invalid_time":"2022-07-22T16:19:41+0800"},{"hcs_sid":"2022072216012700001","hcs_token":"9f2d802c8f8854f82cc9ea9e5390c26f","init_time":"2022-07-22T16:01:27+0800","invalid_time":"2022-07-22T17:18:27+0800"}],"code":200}'
+    remove_expired_task
 
-my_log debug $response
-code=$(echo $response | jq -r .code)
-if [ $code -eq 200 ]; then
-    #XXX add EX to purge expire token?
+    tasks=$(curl -s -H "ACCESSTOKEN:${accesstoken}" -H "ACCESSTOKEN-AP:${accesstokenAp}" -X GET "${rootUrl}/${hcsPath}?ap_wallet=${kapWallet}")
+    [ $DEBUG -eq 1 ] && tasks='{"hcs":[{"hcs_sid":"2022072215014100001","hcs_token":"94efde4c624ce129eab0756b52897de3","init_time":"2022-07-22T15:01:41+0800","invalid_time":"2022-07-22T16:19:41+0800"},{"hcs_sid":"2022072216012700001","hcs_token":"9f2d802c8f8854f82cc9ea9e5390c26f","init_time":"2022-07-22T16:01:27+0800","invalid_time":"2022-07-22T17:18:27+0800"}],"code":200}'
 
-    lastOne=$(redis-cli lindex boss.hcs.token.list -1 | jq -r .hcs_sid)
-    [ -z "${lastOne}" -o "Xnull" = "X${lastOne}" ] && lastOne=0
+    code=$(echo $tasks | jq -r .code)
+    hcs=$(echo $tasks | jq -cr .hcs[])
+    if [ "X$code" = "X200" -a -n "$hcs" ]; then
+        #XXX add EX to purge expire token?
 
-    echo $response | jq -cr .hcs[] | \
-        while read item; do
-            if [ $(echo $item | jq -r .hcs_sid) -gt ${lastOne} ]; then
-                echo "=>$item<="
-                redis-cli rpush boss.hcs.token.list "$item"
-            else
-                my_log info "item - ${item} already in"
-            fi
-        done
+        lastOne=$(redis-cli LINDEX boss.hcs.token.list -1 | jq -r .hcs_sid)
+        [ -z "${lastOne}" -o "Xnull" = "X${lastOne}" ] && lastOne=0
 
-    #redis-cli SET boss.hcs.token $response
-    #my_log debug redis-cli SET boss.hcs.token $response
-#else
-#    my_log debug redis-cli DEL boss.hcs.token
-#    redis-cli DEL boss.hcs.token
-fi
+        echo $tasks | jq -cr .hcs[] | \
+            while read item; do
+                if [ $(echo $item | jq -r .hcs_sid) -gt ${lastOne} ]; then
+                    redis-cli RPUSH boss.hcs.token.list "$item"
+                else
+                    my_log info "item - ${item} already in"
+                fi
+            done
+
+        #redis-cli SET boss.hcs.token $tasks
+        #my_log debug redis-cli SET boss.hcs.token $tasks
+        #else
+        #    my_log debug redis-cli DEL boss.hcs.token
+        #    redis-cli DEL boss.hcs.token
+        true
+    else
+        false
+    fi
+}
+
+get_main
--- system/fika-manager/files/hcs_honest_challenge.sh
+++ system/fika-manager/files/hcs_honest_challenge.sh
@@ -2,59 +2,95 @@
 
 DEBUG=0
 
+rootUrl=""
+accesstoken=""
+apTokenPath=""
+hcsPath=""
+kapWallet=""
+accesstokenAp=""
+
 my_log() {
     level=$1 && shift
 
     if [ -e /dev/log ]; then
-        logger -s -t fika-manager -p ${level} "$@"
+        logger -s -t hcs-honest-challenge -p ${level} "$@"
     else
-        echo "[fika-manager][${level}] $@"
+        echo "[hcs-honest-challenge][${level}] $@"
+    fi
+}
+
+db_fetch() {
+    local kap_core kap_boss
+
+    kap_core=$(redis-cli --raw GET kap.core)
+    kap_boss=$(redis-cli --raw GET kap.boss)
+
+    rootUrl=$(echo $kap_boss | jq -r .root_url)
+    accesstoken=$(echo $kap_boss | jq -r .access_token)
+    apTokenPath=$(echo $kap_boss | jq -r .ap_token_path)
+    hcsPath=$(echo $kap_boss | jq -r .hcs_path)
+    kapWallet=$(echo $kap_core | jq -r .wallet_address)
+    accesstokenAp=$(redis-cli --raw GET kap.boss.ap.token)
+
+    my_log debug "appUrl: $rootUrl"
+    my_log debug "kapWallet: $kapWallet"
+    my_log debug "accesstoken: $accesstoken"
+    my_log debug "accesstokenAp: $accesstokenAp"
+
+    [ -z "$rootUrl" -o -z "$accesstoken" -o -z "$accesstokenAp" -o -z "$kapWallet" ] && \
+        my_log error "database rootUrl/accesstoken/accesstokenAp/kapWallet=${rootUrl}/${accesstoken}/${accesstokenAp}/${kapWallet} invalid" && \
+        exit 127
+
+    true
+}
+
+report_boss_hcs() {
+    local cid hashed changed json tid
+
+    cid=$1 && shift
+    hashed=$1 && shift
+    tid=$1 && shift
+
+    json=$(jq -rcM --null-input \
+        --arg wallet "$cid" \
+        --arg hashed "$hashed" \
+        '{ "app_wallet": $wallet, "hashed": $hashed}')
+
+    response=$(curl -s -H "ACCESSTOKEN:${accesstoken}" -H "ACCESSTOKEN-AP:${accesstokenAp}" -X POST -d $json "${rootUrl}/${hcsPath}?ap_wallet=${kapWallet}")
+    code=$(echo $response | jq -r .code)
+    if [ "X$code" = "X200" ]; then
+        changed=$(echo $challenger | jq -rcM --argjson send true '.send = $send')
+        redis-cli HSET boss.hcs.challengers.${tid} ${cid} ${changed}
+        my_log debug "redis-cli HSET boss.hcs.challengers.${tid} ${cid} ${changed}"
+        true
+    else
+        my_log error "POST $json ${rootUrl}/${hcsPath}?ap_wallet=${kapWallet} fail"
+        false
     fi
 }
 
 # curl -s -H 'ACCESSTOKEN:ce18d7a0940719a00da82448b38c90b2' -X GET "https://oss-api.k36588.info/v0/ap/ap_token?ap_wallet=0x365962cd383a593975E743fD59Bbf2C1Bc141CF5"
 #{"ap_token":"02ec7a905b70689d9b30c6118fd1e62f","code":200}⏎ 
 
-challengeId=$1 && shift
+post_main() {
+    local cid tid
 
-kap_core=$(redis-cli --raw GET kap.core)
-kap_boss=$(redis-cli --raw GET kap.boss)
+    cid=$1 && shift
 
-rootUrl=$(echo $kap_boss | jq -r .root_url)
-accesstoken=$(echo $kap_boss | jq -r .access_token)
-apTokenPath=$(echo $kap_boss | jq -r .ap_token_path)
-hcsPath=$(echo $kap_boss | jq -r .hcs_path)
-kapWallet=$(echo $kap_core | jq -r .wallet_address)
-accesstokenAp=$(redis-cli --raw GET kap.boss.ap.token)
+    tid=$(redis-cli LINDEX boss.hcs.token.list 0 | jq -r .hcs_sid)
+    [ -z "${tid}" -o "Xnull" = "X${tid}" ] \
+        && my_log error "not task for ${cid}" \
+        && exit 127
 
-my_log debug "challengeId: $challengeId"
-my_log debug "appUrl: $rootUrl"
-my_log debug "kapWallet: $kapWallet"
-my_log debug "accesstoken: $accesstoken"
-my_log debug "accesstokenAp: $accesstokenAp"
+    challenger=$(redis-cli --raw HGET boss.hcs.challengers.${tid} ${cid})
+    if [ -z "$challenger" ]; then
+        my_log debug "No any challengers in this task-${tid}"
+        exit 0
+    fi
 
-[ -z "$rootUrl" -o -z "$accesstoken" -o -z "$accesstokenAp" -o -z "$kapWallet" ] && exit 127
+    hashed=$(echo $challenger | jq -r .hashed)
 
-nowOne=$(redis-cli LINDEX boss.hcs.token.list 0 | jq -r .hcs_sid)
-[ -z "${nowOne}" -o "Xnull" = "X${nowOne}" ] \
-    && my_log error "not task for ${challengeId}" \
-    && exit 127
+    db_fetch && report_boss_hcs "$cid" "$hashed" "$tid"
+}
 
-challenger=$(redis-cli --raw HGET boss.hcs.challengers.${nowOne} ${challengeId)
-hashed=$(echo $challenger | jq -r .hashed)
-
-json=$(jq -rcM --null-input \
-    --arg wallet "$challengeId" \
-    --arg hashed "$hashed" \
-    '{ "app_wallet": $wallet, "hashed": $hashed}')
-
-response=$(curl -s -H "ACCESSTOKEN:${accesstoken}" -H "ACCESSTOKEN-AP:${accesstokenAp}" -X POST -d $json "${rootUrl}/${hcsPath}?ap_wallet=${kapWallet}")
-my_log debug $response
-code=$(echo $response | jq -r .code)
-if [ $code -eq 200 ]; then
-    changed=$(echo $challenger | jq -rcM --argjson send true '.send = $send')
-    redis-cli HSET boss.hcs.challengers.${nowOne} ${challengeId} ${changed}
-    my_log debug "redis-cli HSET boss.hcs.challengers.${nowOne} ${challengeId} ${changed}"
-else
-    my_log error "POST $json ${rootUrl}/${hcsPath}?ap_wallet=${kapWallet} fail"
-fi
+[ $# -gt 0 ] && post_main $@
